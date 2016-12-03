da5749a895883b2c9a63fa94a1a092059ea1c5b5
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-03 11:38:46 +0530

    Choosing an Index

---
+++ Chap5/11 Choosing an Index/choosing_an_index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vicky on 12/3/16.
+ */
+
+
+/*
+*   Winning query plan is cached in memory
+*   Runs all query with all 3 indexes.
+*   Then sorts the query based on the best performance.
+*
+* */
