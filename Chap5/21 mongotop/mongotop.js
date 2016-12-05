61805326d396b7ed72c13b394058c0dc372065dd
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-05 11:32:20 +0530

    Mongotop

---
+++ Chap5/21 mongotop/mongotop.js
@@ -0,0 +1,21 @@
+/**
+ * Created by vicky on 12/3/16.
+ */
+
+/*
+    Recap:
+    =====
+
+        Indexes are critical to performance
+        Explain
+        Hint
+        Profiling
+
+*   Mongotop:
+*   =========
+*
+*       named after UNIX top command
+*
+*       mongotop 5
+*
+* */
