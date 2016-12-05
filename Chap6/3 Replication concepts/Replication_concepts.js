24813908458e29201b3d627b90cc520f9b538752
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-05 11:35:19 +0530

    Replication

---
+++ Chap6/3 Replication concepts/Replication_concepts.js
@@ -0,0 +1,26 @@
+/**
+ * Created by vicky on 12/4/16.
+ */
+
+/*
+*   Replica Sets:
+*   =============
+*
+*   P => S
+*   Replication Sets are replicated clusters.
+*
+*   Automatic Failover:
+*   ===================
+*
+*   Automatic Failover
+*   Automatic Node Recovery
+*
+*
+*   Recovery:
+*   ========
+*
+*   Rollback writes that didn't make it and archive it.
+*
+*
+*
+* */
