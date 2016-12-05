eb359e6eb5078aa3d1b862014a9babd1fca62729
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-05 11:34:27 +0530

    Replication Overview

---
+++ Chap6/1 Replication overview/replication_overview.js
@@ -0,0 +1,31 @@
+/**
+ * Created by vicky on 12/4/16.
+ */
+
+
+/*
+* Replication in MongoDB
+*
+*   Redundant copies across multiple machines.
+*
+*   Replication:
+*   ============
+*
+*       High Availability(Failover)
+*
+*       Data Safety(Durability)
+*
+*       Extra Copies
+*
+*       Disaster Recovery
+*
+*       One Primary => Multiple Secondaries
+*       Read from Primary or secondaries
+*
+*
+*
+*
+*
+* */
+
+
