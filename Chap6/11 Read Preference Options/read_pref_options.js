7ef84a529fb421b3c1efa97eccc4ed5475a89180
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:50:14 +0530

    Read Preference Options

---
+++ Chap6/11 Read Preference Options/read_pref_options.js
@@ -0,0 +1,36 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+
+/*
+*   Read Preference Options:
+*   ========================
+*
+*      Primary (Default)
+*
+*      Primary Preferred
+*
+*      Secondary
+*
+*      Secondary Preferred
+*
+*      Nearest
+*
+*
+*      When to use what?
+*      =================
+*
+*      when in doubt, use primary
+*
+*      when in remote, use nearest
+*
+*      Use secondary, for certain reporting workloads
+*
+*      Use nearest, for read loads
+*
+*      Secondary Preferred, gives an even split of data.
+*
+*
+*
+* */
