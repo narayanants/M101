6b82286755ffa73951ce94c8a9a33c6c4abe87ab
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:43:11 +0530

    Voting Options

---
+++ Chap7/5 Voting Options/voting_options.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+/*
+* Voting Options:
+* ===============
+*
+*   Don't use votes
+*
+*   votes: <n>
+*
+*       More votes = Eligible for Primary.
+*
+*
+*
+*
+* */
