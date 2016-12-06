8f3f136deb649d80b62fb1817a10df5dcac964f0
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:47:03 +0530

    Examining w Parameters

---
+++ Chap7/8 Examining the w parameters/examine_w_params.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+/*
+*  Examining w params:
+*  ==================
+*
+*   no call to getLastError
+*
+*   w:1
+*
+*   w:majority
+*
+*   w:3 <= RF=3
+*
+*
+* */
