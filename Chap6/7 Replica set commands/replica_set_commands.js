fc2f32fd89700ff6b8a7b44d43b6c2eb48a103d5
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:49:14 +0530

    Replica Set Commands

---
+++ Chap6/7 Replica set commands/replica_set_commands.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vicky on 12/4/16.
+ */
+
+/*
+*   Replica Set Commands:
+*   =====================
+*
+*   rs.conf();
+*	rs.reconfig();
+*	rs.stepDown();
+*   rs.syncFrom();
+*   rs.freeze(); // node ineligible to become primary for certain time.
+*
+*
+* */
