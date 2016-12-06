30f200c87f0fbe136a470670c279e390130ce89b
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:43:26 +0530

    Applied Reconfig

---
+++ Chap7/6 Applied Reconfig/applied_reconfig.js
@@ -0,0 +1,30 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+/*
+*   var cfg = rs.conf();
+*
+*   cfg.members[2].slaveDelay = true;
+*
+*   rs.reconfig(cfg);
+*
+*   cfg.members[2].priority = 0;
+*
+*   rs.reconfig(cfg);
+*
+*   cfg.members[2].slaveDelay = 8 * 3600;
+*
+*   rs.reconfig(cfg);
+*
+*   rs.isMaster();
+*
+*   use local
+*
+*   db.oplog.rs.find().sort({$natural:-1}).limit(5);
+*
+*
+*
+*
+*
+* */
