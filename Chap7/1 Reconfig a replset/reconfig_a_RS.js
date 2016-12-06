8f3a8b349fae01c086a13a2aa4c2fa994d08b767
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:42:03 +0530

    Reconfig a ReplSet

---
+++ Chap7/1 Reconfig a replset/reconfig_a_RS.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+/*
+*   Rconfig a Replset:
+*   =================
+*
+*   rs.reconfig()
+*
+*   IN a 3 member RS, if one goes down?, we can reconfig
+*
+*   send this command to the primary of the set.
+*
+*   local.system.replset.find().pretty()
+*
+*
+* */
