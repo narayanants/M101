019c6696a4c8cb98e05dedf9d42d797b4757f792
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:42:36 +0530

    Priority Options

---
+++ Chap7/3 Priority Options/priority_options.js
@@ -0,0 +1,21 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+
+/*
+*   Priority Options:
+*   =================
+*
+*   Used as bias to which server becomes primary.
+*
+*   priority: 1 (default)
+*
+*   priority: 0 (never primary)
+*
+*
+*
+*
+*
+*
+* */
