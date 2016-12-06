efa681813bd52135ff9999d9160ee6b4a0a547a1
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:42:52 +0530

    Hidden and Slave Delay

---
+++ Chap7/4 Hidden and Slave Delay/hidden_and_slave_delay.js
@@ -0,0 +1,15 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+/*
+*  Hidden and Slave Delay Options:
+*  ===============================
+*
+*   hidden: <bool> // to never query a secondary
+*
+*   slaveDelay: <seconds> // member must lag/ never be fresher than delay time.
+*
+*
+*   slaveDelay:8 * 3600
+* */
