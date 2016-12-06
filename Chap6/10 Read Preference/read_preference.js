bf515161e43896aede03f33f6dd3e53b6a71f463
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:50:02 +0530

    Read Preference

---
+++ Chap6/10 Read Preference/read_preference.js
@@ -0,0 +1,28 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+/*
+*   Read Preference
+*   ===============
+*
+*   rs.slaveOK()
+*
+*   why query a secondary?
+*
+*       Geographical location
+*
+*       Separate workload( Analytics Server)
+*
+*       Separate Load
+*
+*       Unavailablility
+*
+*       Potential stale data on secondaries (Potential consistent)
+*
+*       Did you test a secondary?
+*
+*
+*
+*
+* */
