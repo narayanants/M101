f777ac173af91a9b4670e7703c51e5881425166b
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-06 11:42:21 +0530

    Arbiters

---
+++ Chap7/2 Arbiters/arbiters.js
@@ -0,0 +1,29 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+
+/*
+*   Arbiters:
+*   =========
+*
+*   arbiterOnly:true
+*
+*   Arbiter has no data
+*
+*   Arbiter used only for voting purpose.
+*
+*
+*   When to use an Arbiter?
+*
+*   => to make an odd no of votes in a RS.
+*
+*   => to spread RS over DC's
+*
+*   => to protect against n/w splits.
+*
+*
+*
+*
+*
+* */
