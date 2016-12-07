6f45c0350363ef2120c45ef69382e2473a99eb07
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-07 11:03:06 +0530

    Recommended Settings for a RS in 1 DC

---
+++ Chap7/10 Recommended settings for RS/recommended_settings_for_RS_in_single_DC.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vicky on 12/7/16.
+ */
+/*
+*
+*  Recommended Settings for RS in 1 DC:
+*  ===================================
+*
+*  3 Member RS => 2 data clusters + 1 Arbiter.
+*
+*  Recommended Settings for RS in 3 DC:
+*  ====================================
+*
+*  3 Members RS => 2 data clusters in 2 DC + 1 Arbiter in 1 DC.
+*
+*
+*
+* */
