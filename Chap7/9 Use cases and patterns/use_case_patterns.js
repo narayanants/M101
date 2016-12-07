84a74e0facfbdab0625946eadd2b06ff3dffc9a6
Narayanan Ts <tsnarayanan7@gmail.com>
2016-12-07 11:04:02 +0530

    USe Case Patterns

---
+++ Chap7/9 Use cases and patterns/use_case_patterns.js
@@ -0,0 +1,26 @@
+/**
+ * Created by vicky on 12/6/16.
+ */
+
+
+/*
+*
+*   w:'majority' =>  most things important
+*
+*   w:all        =>  flow control
+*
+*
+*   use write concern
+*
+*   Use w majority
+*
+*   Tune if slow
+*
+*   Call GLE when job ends
+*
+*   getLastError(<wval>,<timeout>)
+*
+*   {w : 'majority', wtimeout: 8000}
+*
+*
+* */
