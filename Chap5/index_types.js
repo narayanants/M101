2e3479b666e557e3cb438c36a32d92f492eb9891
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-10 21:31:47 +0530

    Index types changes

--- Chap5/index_types.js
+++ Chap5/index_types.js
@@ -1,3 +1,20 @@
+/*
+    Indexes Types:
+    --------------
+
+        * compound index
+        * multikey index
+        * unique index
+        * sparse index
+        * background index
+        * Geospatial index
+        * Geospherical index
+        * Text index
+
+*/
+
+
+
 //creating indexes
 
 db.students.createIndex({"student_id":1,"class_id":1});
@@ -86,6 +103,72 @@ exp.find({i:45,j:23},{_id:0,i:1,j:1,k:1});
     db.students.ensureIndex({location:'2d',type:1});
     db.students.find({location:{$near:[x,y]}}).limit(20);
     db.students.find({location:{$near:[50,50]}});
+
+ // Geospherical indexes
+
+    db.students.ensureIndex({location:'2dsphere'});
+    db.places.find({location:{
+        $near:{
+            $geometry:{type:"Point",coordinates:[50,50]},
+            $maxDistance:2000
+        }
+    }});
+
+// Text indexes
+    db.sentences.ensureIndex({'words':'text'});
+    db.sentences.find({$text:{$search:'dog'}});
+    db.sentences.find({$text:{$search:'dog tree obsidian'}},{score:{$meta:'textScore'}}).sort({score:{$meta:'textScore'}});
+
+
+
+/*  
+    Profiling
+    ---------
+
+        Profiling is a service that R/W to system.profile collection
+
+        3 levels
+
+            0 - default
+            1 - slow queries
+            2 - all queries (Debugging)
+
+            mongod --dbpath /usr/local/var/mongodb --profile 1 --slowms 2
+
+            db.system.profile.find().pretty()
+*/
+
+    db.system.profile.find({millis:{$gt:1}}).sort({ts:1}).pretty()
+    db.getProfilingLevel()  // 1
+    db.getProfilingStatus()
+    db.setProfilingStatus(1,4)
+    
+
+    /* 
+        Mongotop:
+        =========
+
+        mongotop 5
+
+        Mongostat:
+        ==========
+
+        mongostat --port 27018
+
+    */
+
+
+    /* 
+        Sharding overview:
+        ------------------
+
+            * Sharding is splitting of large data sets across multiple servers
+            Application => mongos => mongod => Replica Sets
+
+    
+    */
+
+
 
 
  
