5cdeb35f453f5448ffcf5f0e3633183936af8a6a
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-10 21:37:57 +0530

    Replication Concepts overview

---
+++ Chap6/replication_concepts.js
@@ -0,0 +1,18 @@
+/* 
+    Replication in MongoDB:
+    ----------------------
+
+    * Redundant copies of data across multiple machines.
+    
+    Replication:
+    -----------
+        * High Availability (Failover)
+        * Data Safety (Durability)
+        * Extra copies
+        * Disaster Recovery
+
+        1 Primary => Multiple secondaries
+
+        Read from Primary/secondaries
+        
+*/
