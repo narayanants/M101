0a713e3082b3105b299a7bd98870d0fae8ed6f54
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-20 12:56:22 +0530

    changes to elementOperators

--- Chap2/4 element_operators/elementOperators.js
+++ Chap2/4 element_operators/elementOperators.js
@@ -1,6 +1,6 @@
-db.moviesDetails.find({ "tomato.meter": { $exists: true } })
+db.movieDetails.find({ "tomato.meter": { $exists: true } })
 
-db.moviesDetails.find({ "tomato.meter": { $exists: false } })
+db.movieDetails.find({ "tomato.meter": { $exists: false } })
 
 // Value of $type may be either a BSON type number or the string alias
 // See https://docs.mongodb.org/manual/reference/operator/query/type
@@ -13,3 +13,5 @@ db.moviesDetails.find({"tomato.meter":{$exists:false}})
 
 
 db.moviesScratch.find({_id:{$type:"string"}});
+
+db.movieDetails.find({"tomato.meter":{$exists:true}})
