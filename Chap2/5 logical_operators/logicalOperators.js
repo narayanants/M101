65dba46c52d97fd637114ec62b05837e85e80738
Narayanan Ts <tsnarayanan7@gmail.com>
2017-01-20 13:02:02 +0530

    Changes made to logicalOperators

--- Chap2/5 logical_operators/logicalOperators.js
+++ Chap2/5 logical_operators/logicalOperators.js
@@ -37,5 +37,10 @@ db.movieDetails.find({$and:[{"metacritic":{$ne:null}},{"metacritic":{$gt:95}}]})
 */
 
 
+db.movieDetails.find({$or:[{"tomato.meter":{$gt:99}},{"metacritic":{$gt:95}}]}).count()
+
+db.movieDetails.find({$and:[{"tomato.meter":{$gt:95}},{"tomato.meter":{$exists:true}}]}).count()
+
+
 
 
